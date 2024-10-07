e88482a109a1240f8344d91682b481fe858a133a
Crisstuff <cristofferfolkedalrramsdal@gmail.com>
2024-10-07 16:22:50 +0200

    start på øvelses oppgave 1 2.2.B

---
+++ 2.2.B_ovelseoppgaver/ovelse.1/tekstverktoy.py
@@ -0,0 +1,6 @@
+#!/bin/bash
+import tekstverktøy
+tekst = "Hello World!"
+print(tekstverktøy.tell_vokaler(tekst))  # Output: 3
+print(tekstverktøy.speilvend(tekst))  # Output: !dlroW olleH
+print(tekstverktøy.erstatt_vokal(tekst, 'i'))  # Output: Hilli Wirld!
